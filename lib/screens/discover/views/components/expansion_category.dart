ecfa9e5c3ed6a4cc1d5fdadcb5ac8f485be8cc3a
Martin SangDo <martin.sangdo@gmail.com>
2025-01-03 02:35:44 +0700

    - done category page

--- lib/screens/discover/views/components/expansion_category.dart
+++ lib/screens/discover/views/components/expansion_category.dart
@@ -5,53 +5,58 @@ import 'package:shop/route/screen_export.dart';
 import '../../../../constants.dart';
 
 class ExpansionCategory extends StatelessWidget {
-  const ExpansionCategory({
+  ExpansionCategory({
     super.key,
     required this.title,
-    required this.subCategory,
-    required this.svgSrc,
+    this.subCategory,
+    this.svgSrc,
   });
 
-  final String title, svgSrc;
-  final List subCategory;
+  final String title;
+  String? svgSrc;
+  List? subCategory;
 
   @override
   Widget build(BuildContext context) {
     return ExpansionTile(
       iconColor: Theme.of(context).textTheme.bodyLarge!.color,
       collapsedIconColor: Theme.of(context).textTheme.bodyMedium!.color,
-      leading: SvgPicture.asset(
-        svgSrc,
-        height: 24,
-        width: 24,
-        colorFilter: ColorFilter.mode(
-          Theme.of(context).iconTheme.color!,
-          BlendMode.srcIn,
-        ),
-      ),
+      // leading: SvgPicture.asset(
+      //   svgSrc,
+      //   height: 24,
+      //   width: 24,
+      //   colorFilter: ColorFilter.mode(
+      //     Theme.of(context).iconTheme.color!,
+      //     BlendMode.srcIn,
+      //   ),
+      // ),
       title: Text(
         title,
         style: const TextStyle(fontSize: 14),
       ),
       textColor: Theme.of(context).textTheme.bodyLarge!.color,
       childrenPadding: const EdgeInsets.only(left: defaultPadding * 3.5),
-      children: List.generate(
-        subCategory.length,
+      children: (subCategory!.isNotEmpty) ? List.generate(
+        subCategory!.length,
         (index) => Column(
           children: [
             ListTile(
               onTap: () {
-                Navigator.pushNamed(context, onSaleScreenRoute);
+                Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => BookmarkScreen(appBarTitle: subCategory![index].title, pageType: 'category')
+                          ));
               },
               title: Text(
-                subCategory[index].title,
+                subCategory![index].title,
                 style: const TextStyle(fontSize: 14),
               ),
             ),
-            if (index < subCategory.length - 1) const Divider(height: 1),
+            if (index < subCategory!.length - 1) const Divider(height: 1),
           ],
         ),
-      ),
+      ) : [],
     );
   }
 }
--- lib/screens/discover/views/discover_screen.dart
+++ lib/screens/discover/views/discover_screen.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:shop/constants.dart';
 import 'package:shop/models/category_model.dart';
+import 'package:shop/models/database_helper.dart';
 import 'package:shop/route/route_constants.dart';
 import 'package:shop/screens/search/views/components/search_form.dart';
 
@@ -15,6 +18,7 @@ class DiscoverScreen extends StatefulWidget {
 }
 
 class _PageState extends State<DiscoverScreen> {
+  List<CategoryModel> _categoryTree = [];
 //setup Bottom Bar
   int _selectedBottomIndex = 1;
   
@@ -24,13 +28,46 @@ class _PageState extends State<DiscoverScreen> {
     });
     if (context.mounted){
       if (index == 0){
-        Navigator.pushNamed(context, homeScreenRoute);  //Catetories pages
+        Navigator.pushNamed(context, homeScreenRoute);  //Home page
       }
     }
   }
+  //
+  void _fetchMetadata() async{
+    final _metadata = await DatabaseHelper.instance.rawQuery('SELECT categories FROM metadata', []);
+    if (_metadata.isNotEmpty){
+      List<dynamic> categories = jsonDecode(_metadata[0]['categories']);
+      List<CategoryModel> categoryTree = [];
+      Map<String, List<CategoryModel>> catMap = {};  //key: category name, value: list of sub cat
+      //compose cat & sub cat list
+      for (dynamic cat in categories){
+        if (catMap[cat['name']] == null && cat['parent'] == null){
+            catMap[cat['name']] = []; //init list
+        }
+        if (cat['parent'] != null){
+          //this is a sub cat
+          if (catMap[cat['parent']] == null){
+            catMap[cat['parent']] = []; //init list
+          }
+          List<CategoryModel>? subCat = catMap[cat['parent']];
+          subCat?.add(CategoryModel(title: cat['name']));
+        }
+      }
+      //
+      for (String cat_name in catMap.keys){
+        categoryTree.add(CategoryModel(title: cat_name, subCategories: catMap[cat_name]));
+      }
+      //
+      setState(() {
+        _categoryTree = categoryTree;
+      });
+    }
+  }
+  //
   @override
   void initState() {
       super.initState();
+      _fetchMetadata();
   }
 
   @override
@@ -63,11 +100,10 @@ class _PageState extends State<DiscoverScreen> {
             // ),
             Expanded(
               child: ListView.builder(
-                itemCount: demoCategories.length,
+                itemCount: _categoryTree.length,
                 itemBuilder: (context, index) => ExpansionCategory(
-                  svgSrc: demoCategories[index].svgSrc!,
-                  title: demoCategories[index].title,
-                  subCategory: demoCategories[index].subCategories!,
+                  title: _categoryTree[index].title,
+                  subCategory: _categoryTree[index].subCategories!,
                 ),
               ),
             )
